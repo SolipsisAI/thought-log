a3acf3ea21e201820a580a37c1f0a0f14a7207f0
Bitjockey <bitjockey@jackpoint.network>
2022-06-26 17:21:33 -0400

    Add cli for configuring settings

--- src/thought_log/cli.py
+++ src/thought_log/cli.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 import click
 
 
@@ -7,12 +10,34 @@ def cli():
 
 
 @cli.command()
-@click.argument("text")
-def add(text):
+@click.option("--text", "-t")
+@click.option("--filename", "-f")
+def add(text, filename):
     """Add a new entry to the log"""
     from thought_log.utils import DATA_DIR, ROOT_DIR
 
     print(f"Input: {text}|{ROOT_DIR} {DATA_DIR}")
+
+
+@cli.command()
+@click.option("--storage_dir", "-d", default=os.getenv("TL_STORAGE_DIR", None))
+@click.option("--overwrite/--no_overwrite", default=False)
+def configure(storage_dir, overwrite):
+    from thought_log.utils import update_config, read_config
+
+    config = read_config()
+
+    if not storage_dir:
+        storage_dir = click.prompt("Where do you want files to be stored? ")
+
+    if "storage_dir" not in config or overwrite:
+        config["storage_dir"] = storage_dir
+
+    if not Path(storage_dir).exists():
+        click.echo(f"{storage_dir} doesn't yet exist; created.")
+        Path(storage_dir).mkdir(parents=True)
+
+    update_config(config)
 
 
 @cli.command()
