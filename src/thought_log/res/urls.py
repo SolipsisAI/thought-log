ea2fefa2e58651a8d0d5999cdef23f8dd7f202ea
Bitjockey <bitjockey@jackpoint.network>
2022-06-28 05:55:36 -0400

    Change filename

--- src/thought_log/res/urls.py
+++ src/thought_log/res/urls.py
@@ -5,7 +5,7 @@ MODELS = {
     },
     "classifier": {
         "source": "solipsis",
-        "url": "https://solipsisai.us-east-1.linodeobjects.com/models/distilroberta-finetuned.tar.gz",
+        "url": "https://solipsisai.us-east-1.linodeobjects.com/models/distilroberta-finetuned__exported.tar.gz",
     },
     "emotion_classifier": {
         "source": "huggingface",
