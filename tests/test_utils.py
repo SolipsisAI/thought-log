60226e61922d059bbd7425ee3ced4c37fe2918fa
Bitjockey <bitjockey@jackpoint.network>
2022-06-28 14:33:43 -0400

    Create fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,6 +10,16 @@ APP_NAME = "ThoughtLog"
 APP_AUTHOR = "SolipsisAI"
 
 
+@pytest.fixture
+def mock_classifier():
+    def mock_classify(text, *args, **kwargs):
+        return f"LABEL {text}"
+
+    _mock = Mock()
+    _mock.classify = mock_classify
+    yield _mock
+
+
 @pytest.mark.parametrize("path_fn,expected", [
     (paths.config_path, Path(user_config_dir(APP_NAME, APP_AUTHOR))),
     (paths.cache_path, Path(user_cache_dir(APP_NAME, APP_AUTHOR))),
@@ -23,9 +33,5 @@ def test_preprocess_text_no_classifier():
     assert common.preprocess_text("testing") == "testing"
 
 
-def test_preprocess_text_with_classifier():
-    def mock_classify(text, *args, **kwargs):
-        return f"LABEL {text}"
-    classifier_mock = Mock()
-    classifier_mock.classify = mock_classify
-    assert classifier_mock.classify("Test") == "LABEL Test"
+def test_preprocess_text_with_classifier(mock_classifier):
+    assert mock_classifier.classify("Test") == "LABEL Test"
