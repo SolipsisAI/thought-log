0494c5ef002a77b06114b27746301ed16e66378d
Bitjockey <fastjack@jackpoint.network>
2022-06-28 23:46:31 -0400

    Simplify file listing

--- src/thought_log/importer/google_drive.py
+++ src/thought_log/importer/google_drive.py
@@ -1,6 +1,9 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
+QUERY = "'{folder_id}' in parents and trashed=false {mimetype}"
+FOLDER_MIMETYPE = "application/vnd.google-apps.folder"
+
 
 def authenticate():
     # You need client_secrets.json
@@ -8,38 +11,12 @@ def authenticate():
     gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
 
 
-def list_dirs(folder_name: str = None, subfolder_name: str = None):
-    gauth = GoogleAuth()
-    drive = GoogleDrive(gauth)
+def list_contents(*, folder_id: str = "root", content_type: str = None):
+    drive = GoogleDrive(GoogleAuth())
 
-    folder_type = "mimeType = 'application/vnd.google-apps.folder'"
-    file_type = "mimeType != 'application/vnd.google-apps.folder'"
+    cond = "!=" if content_type == "file" else "="
+    mimetype = f"and mimetype {cond} {FOLDER_MIMETYPE}" if content_type else ""
 
-    query = "'{folder_id}' in parents and trashed=false and {mimetype}"
-    folders = drive.ListFile(
-        {"q": query.format(folder_id="root", mimetype=folder_type)}
-    ).GetList()
-
-    if not folder_name:
-        return folders
-
-    for folder in folders:
-        if folder["title"] == folder_name:
-            break
-
-    folder_id = folder["id"]
-    subfolders = drive.ListFile(
-        {"q": query.format(folder_id=folder_id, mimetype=folder_type)}
-    ).GetList()
-
-    if not subfolder_name:
-        return subfolders
-
-    for subfolder in subfolders:
-        if subfolder["title"] == subfolder_name:
-            break
-
-    subfolder_id = subfolder["id"]
     return drive.ListFile(
-        {"q": query.format(folder_id=subfolder_id, mimetype=file_type)}
+        {"q": QUERY.format(folder_id=folder_id, mimetype=mimetype)},
     ).GetList()
