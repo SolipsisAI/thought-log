dda1b0f18cfc0242d5f1e13dc576bf33f917a9cf
Bitjockey <fastjack@jackpoint.network>
2022-06-28 23:03:26 -0400

    List subfolders

--- src/thought_log/importer/google_drive.py
+++ src/thought_log/importer/google_drive.py
@@ -8,11 +8,23 @@ def authenticate():
     gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
 
 
-def list_dirs():
-    drive = GoogleDrive(GoogleAuth())
+def list_dirs(title: str = None):
+    gauth = GoogleAuth()
+    drive = GoogleDrive(gauth)
     dirs = drive.ListFile(
         {
             "q": "'root' in parents and trashed=false and mimeType = 'application/vnd.google-apps.folder'"
         }
     ).GetList()
-    return dirs
+
+    if not title:
+        return dirs
+
+    for d in dirs:
+        if d["title"] == title:
+            break
+
+    folder_id = d["id"]
+    results = drive.ListFile({"q": f"parents in '{folder_id}' and trashed = false"})
+
+    return results.GetList()
