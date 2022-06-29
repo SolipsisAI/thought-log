86b82563e2727a09676c39304fb57cab44d0ae90
Bitjockey <fastjack@jackpoint.network>
2022-06-29 00:02:57 -0400

    Add query builder to handle queries to Google Drive

--- src/thought_log/importer/google_drive.py
+++ src/thought_log/importer/google_drive.py
@@ -1,7 +1,7 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
-QUERY = "'{folder_id}' in parents and trashed=false {mimetype}"
+QUERY = "'{folder_id}' in parents and trashed=false"
 FOLDER_MIMETYPE = "application/vnd.google-apps.folder"
 
 
@@ -20,3 +20,15 @@ def list_contents(*, folder_id: str = "root", content_type: str = None):
     return drive.ListFile(
         {"q": QUERY.format(folder_id=folder_id, mimetype=mimetype)},
     ).GetList()
+
+
+def build_query(
+    *, folder_id: str = "root", content_type: str = None, trashed: str = "false"
+):
+    query_parts = [f"'{folder_id}' in parents", f"trashed={trashed}"]
+
+    if content_type:
+        cond = "=" if content_type == "dir" else "!="
+        query_parts.append(f"mimetype {cond} '{FOLDER_MIMETYPE}'")
+
+    return " and ".join(query_parts)
