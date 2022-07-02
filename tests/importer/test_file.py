c036419e89fd4b3d51fa99db32c12039903f4cd3
Bitjockey <bitjockey@jackpoint.network>
2022-07-02 12:54:39 -0400

    Fix test

--- tests/importer/test_file.py
+++ tests/importer/test_file.py
@@ -1,15 +1,21 @@
+from pathlib import Path
 from unittest.mock import mock_open, patch
 
-import pytest
 import frontmatter
 
 from thought_log.importer import filesystem
 
 
-@patch("builtins.open", mock_open(read_data="test"), create=True)
+def read_data(name):
+    filepath = Path(__file__).parent.parent.joinpath("fixtures", name)
+    with open(filepath) as f:
+        return f.read()
+
+
+@patch("builtins.open", mock_open(read_data=read_data("entry.md")), create=True)
 def test_import_from_file():
     post = filesystem.read_file("test.txt")
-    expected_post = frontmatter.loads("test")
+    expected_post = frontmatter.loads(read_data("entry.md"))
     assert post.content == expected_post.content
 
 
