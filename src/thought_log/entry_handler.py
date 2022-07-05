ee8e06c81367ef64b594c3edca40b392a4ffb9bf
Bitjockey <bitjockey@jackpoint.network>
2022-07-05 01:19:50 -0400

    Move analyzer

--- src/thought_log/entry_handler.py
+++ src/thought_log/entry_handler.py
@@ -1,26 +1,11 @@
-from datetime import datetime
-from json.decoder import JSONDecodeError
-from typing import Dict, Union
+from typing import Union
 
-import frontmatter
-from click.types import Path
-from tqdm.auto import tqdm
-
-from thought_log.config import CLASSIFIER_NAME, EMOTION_CLASSIFIER_NAME, STORAGE_DIR
-from thought_log.nlp.utils import split_paragraphs, tokenize
+from thought_log.config import STORAGE_DIR
 from thought_log.utils import (
     display_text,
-    find_datetime,
-    frequency,
-    get_top_labels,
     hline,
     list_entries,
-    read_csv,
     read_json,
-    snakecase,
-    make_datetime,
-    write_json,
-    zettelkasten_id,
 )
 
 SUPPORTED_EXTS = ["markdown", "md", "txt"]
@@ -40,7 +25,7 @@ def show_entries(reverse: bool, num_entries: int, show_id: bool):
 
         for entry, _ in entries:
             datetime_str = entry["date"]
-            analysis = entry["analysis"]
+            analysis = entry.get("analysis", {})
             labels = analysis.get("labels", {})
 
             # Get emotion
@@ -71,88 +56,3 @@ def load_entries(zkid: Union[str, int]):
 
 def load_entry(filepath):
     return read_json(filepath), filepath
-
-
-def classify_entries(
-    reverse: bool = True,
-    num_entries: int = -1,
-    force: bool = False,
-):
-    from thought_log.nlp.classifier import Classifier
-
-    classifiers = {
-        "emotion": Classifier(
-            model=EMOTION_CLASSIFIER_NAME, tokenizer=EMOTION_CLASSIFIER_NAME
-        ),
-        "sentiment": Classifier(),
-        "context": Classifier(model=CLASSIFIER_NAME, tokenizer=CLASSIFIER_NAME),
-    }
-
-    zkids = list_entries(STORAGE_DIR, reverse=reverse, num_entries=num_entries)
-
-    skipped = 0
-
-    for zkid in tqdm(zkids):
-        entries = load_entries(zkid)
-
-        for entry, filepath in entries:
-            needs_emotion = not bool(entry.get("analysis", {}).get("emotion"))
-            needs_context = not bool(entry.get("analysis", {}).get("context"))
-            needs_analysis = needs_emotion or needs_context
-
-            if not force and not needs_analysis:
-                skipped += 1
-                continue
-
-            paragraph_labels = classify_entry(classifiers, entry["text"])
-
-            emotion_frequency = frequency(paragraph_labels, key="emotion")
-            context_frequency = frequency(paragraph_labels, key="context")
-            sentiment_frequency = frequency(paragraph_labels, key="context")
-
-            # Save labels for each paragraph and their scores
-            stats = {
-                "frequency": {
-                    "emotion": emotion_frequency,
-                    "context": context_frequency,
-                },
-            }
-            top_labels = {
-                "paragraphs": paragraph_labels,
-                "emotion": get_top_labels(emotion_frequency, k=1),
-                "context": get_top_labels(context_frequency, k=3),
-                "sentiment": get_top_labels(sentiment_frequency, k=1),
-            }
-            analysis = {"stats": stats, "labels": top_labels}
-            # Update entry with emotion tag
-            entry["analysis"] = analysis
-            write_json(entry, filepath)
-
-    print(f"Skipped {skipped}")
-
-
-def classify_entry(
-    classifiers,
-    text: str,
-    split: bool = True,
-    emotion_k: int = 1,
-    context_k: int = 3,
-    sentiment_k: int = 1,
-):
-    """Assign emotion classifiers to an entry/text"""
-    doc = tokenize(text)
-
-    classify = lambda t: dict(
-        emotion=classifiers["emotion"].classify(t, k=emotion_k, include_score=True),
-        context=classifiers["context"].classify(t, k=context_k, include_score=True),
-        text=t.strip(),
-        sentiment=classifiers["sentiment"].classify(t, k=sentiment_k, include_score=True),
-    )
-
-    if not split:
-        results = [classify(doc.text)]
-    else:
-        paragraphs = list(map(lambda p: p.text, split_paragraphs(doc)))
-        results = list(map(classify, paragraphs))
-
-    return results
