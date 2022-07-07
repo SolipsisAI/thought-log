55ff734a362e529837992e009943e7eeed4a190c
Bitjockey <bitjockey@jackpoint.network>
2022-07-07 03:54:51 +0000

    Simplify to just emotion classification

--- src/thought_log/analyzer.py
+++ src/thought_log/analyzer.py
@@ -23,15 +23,9 @@ def classify_entries(
 ):
     from thought_log.nlp.classifier import Classifier
 
-    classifiers = {
-        "emotion": Classifier(
-            model=EMOTION_CLASSIFIER_NAME, tokenizer=EMOTION_CLASSIFIER_NAME
-        ),
-        "sentiment": Classifier(
-            model=SENTIMENT_CLASSIFIER_NAME, tokenizer=SENTIMENT_CLASSIFIER_NAME
-        ),
-        "context": Classifier(model=CLASSIFIER_NAME, tokenizer=CLASSIFIER_NAME),
-    }
+    classifier = Classifier(
+        model=EMOTION_CLASSIFIER_NAME, tokenizer=EMOTION_CLASSIFIER_NAME
+    )
 
     zkids = list_entries(STORAGE_DIR, reverse=reverse, num_entries=num_entries)
 
@@ -41,36 +35,13 @@ def classify_entries(
         entries = load_entries(zkid)
 
         for entry, filepath in entries:
-            needs_emotion = not bool(entry.get("analysis", {}).get("emotion"))
-            needs_context = not bool(entry.get("analysis", {}).get("context"))
-            needs_analysis = needs_emotion or needs_context
+            needs_emotion = "emotion" not in entry.get("analysis", {})
 
-            if not force and not needs_analysis:
-                skipped += 1
+            if not needs_emotion and not force:
                 continue
 
-            paragraph_labels = classify_entry(classifiers, entry["text"])
-
-            emotion_frequency = frequency(paragraph_labels, key="emotion")
-            context_frequency = frequency(paragraph_labels, key="context")
-            sentiment_frequency = frequency(paragraph_labels, key="sentiment")
-
-            # Save labels for each paragraph and their scores
-            stats = {
-                "frequency": {
-                    "emotion": emotion_frequency,
-                    "context": context_frequency,
-                },
-            }
-            top_labels = {
-                "paragraphs": paragraph_labels,
-                "emotion": get_top_labels(emotion_frequency, k=1),
-                "context": get_top_labels(context_frequency, k=3),
-                "sentiment": get_top_labels(sentiment_frequency, k=1),
-            }
-            analysis = {"stats": stats, "labels": top_labels}
-            # Update entry with emotion tag
-            entry["analysis"] = analysis
+            emotion = classifier.classify(entry["text"])
+            entry["analysis"] = {"emotion": emotion}
             write_json(entry, filepath)
 
     print(f"Skipped {skipped}")
