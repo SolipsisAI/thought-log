181e248cccc2359eb73f477c2b829448d1282a50
Bitjockey <bitjockey@jackpoint.network>
2022-08-20 16:22:23 -0400

    Automatically retrieve the weather

--- src/thought_log/weather.py
+++ src/thought_log/weather.py
@@ -1,10 +1,23 @@
+from email import message
+from typing import Tuple
+import geocoder
 from pyowm import OWM
 
-from .config import OPENWEATHER_API_KEY, DEFAULT_LOCATION
+from .config import OPENWEATHER_API_KEY
 
 
-def get_weather(location: str = DEFAULT_LOCATION):
+def get_weather(coords: Tuple[float] = None):
+    if not coords:
+        coords = get_location()
+
     owm = OWM(OPENWEATHER_API_KEY)
     mgr = owm.weather_manager()
-    observation = mgr.weather_at_place(location)
+    observation = mgr.weather_at_coords(*coords)
     return observation.weather
+
+
+def get_location():
+    location = geocoder.ip("me")
+    latitude = location.geojson["features"][0]["properties"]["lat"]
+    longitude = location.geojson["features"][0]["properties"]["lng"]
+    return latitude, longitude
