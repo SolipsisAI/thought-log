fcb2f774e18c1a6b2276c049e9c366f669bd51dc
Bitjockey <bitjockey@jackpoint.network>
2022-12-31 23:01:09 -0500

    Sanitize json string

--- src/thought_log/importer/web.py
+++ src/thought_log/importer/web.py
@@ -20,6 +20,7 @@ from thought_log.utils import (
     read_file,
     read_zipfile,
     timestamp,
+    sanitize_json_string,
 )
 
 
@@ -104,6 +105,7 @@ def import_json(data):
         if not title:
             title = datestring(created_datetime)
         entry["title"] = title
+        entry["text"] = sanitize_json_string(entry.get("text", ""))
 
         Note(entry).save()
 
